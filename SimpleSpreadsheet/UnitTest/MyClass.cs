5a8c4d10a9206da3de2ccae335e6da68e33c255d
Jacky Wu <easyfound@126.com>
2018-10-19 22:45:10 +0800

    Initial commit

---
+++ SimpleSpreadsheet/UnitTest/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: lenovo
+ * Date: 2018-10-19
+ * Time: 16:20
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace UnitTest
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
---
+++ SimpleSpreadsheet/UnitTest/Test1.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: lenovo
+ * Date: 2018-10-19
+ * Time: 16:20
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+using SimpleSpreadsheet;
+namespace UnitTest
+{
+	[TestFixture]
+	public class Test1
+	{
+		[Test]
+		public void TestCount()
+		{ 
+			Console.WriteLine("------------------------------------------------------------------".Length.ToString());
+		}
+		[Test]
+		public void TestInput()
+		{ 
+			Command.GetResult("C 20 4");
+		}
+		[Test]
+		public void TestInputEmpty()
+		{
+			 
+			Command.GetResult(string.Empty);
+		}
+		[Test]
+		public void TestSheetsEmpty()
+		{
+			Console.WriteLine();
+			Console.WriteLine(Command.GetResult(string.Empty));
+		}
+	}
+}
